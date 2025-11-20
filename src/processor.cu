800208349900a57c32844fdf7bb6f7c5ff52c957
Yanni Zhuang <zhuangy47@gmail.com>
2025-11-19 20:56:40 -0600

    Decrease staging buffer size

--- src/processor.cu
+++ src/processor.cu
@@ -41,7 +41,7 @@ const int NUM_IO_QUEUES = 16;
 const int NUM_STREAM_BUFFERS = 32;
 const size_t OVERLAP_SIZE = 65536;
 const uint64_t STREAM_CHUNK_SIZE = 32*1024*1024 - OVERLAP_SIZE;
-const uint64_t STAGING_BUFFER_SIZE = 256 * 1024 * 1024;
+const uint64_t STAGING_BUFFER_SIZE = 128 * 1024 * 1024;
 
 const int THREADS_PER_BLOCK = 256;
 
